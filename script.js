2fe19c6edb8afc394a3911611a80a8a94bcdbefb
Shubham <ss9971768704@gmail.com>
2024-02-04 13:49:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const apikey = "035424bbdb89876f58ec6bd7bfa68680";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+    let data = await response.json()
+    
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c" ;
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    }
+    else if(data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+    }
+
+
+    document.querySelector(".weather").style.display = "block"
+
+}
+
+searchBtn.addEventListener("click", () =>{
+    
+    checkWeather(searchBox.value);
+})
