22744d99bde6b1211626f18123f8bbfb8b4571cc
Shubham <ss9971768704@gmail.com>
2024-02-04 18:42:45 +0530

    added responsiveness 2

--- script.js
+++ script.js
@@ -30,6 +30,9 @@ async function checkWeather(city){
     else if(data.weather[0].main == "Mist") {
         weatherIcon.src = "images/mist.png";
     }
+    else if(data.weather[0].main == "Snow"){
+        weatherIcon.src = "images/snow.png";
+    }
 
 
     document.querySelector(".weather").style.display = "block"
